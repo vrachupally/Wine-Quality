dd6c65e0b474e64fe2b74db98eb773503f8040c1
Tara <tara_polli@yahoo.com>
2021-06-02 20:00:36 -0400

    updated app and index

--- backup/app.py
+++ backup/app.py
@@ -1,4 +1,4 @@
-import numpy as np
+# import numpy as np
 from flask import Flask, request, render_template
 import pickle
 
@@ -17,26 +17,48 @@ def predict():
     # x1 = [7.4, 0.7, 0, 1.9, 0.076, 11, 34, 0.9978, 3.51, 0.56, 9.4]
     # x2 = [7.8, 0.88, 0, 2.6, 0.098, 25, 67, 0.9968, 3.2, 0.68, 9.8]
     # x3 = [7.8, 0.76, 0.04, 2.3, 0.092, 15, 54, 0.997, 3.26, 0.65, 9.8]
-
-
-    int_features = [float(x) for x in request.form.values()]
-    # int_features = [float(x) for x1 in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
-
+    print("test wine")
     
-    if prediction==1:
-        return render_template('index.html',
-                               prediction_text='Wine is of LOW quality'.format(prediction),
+    if request.method == 'POST':
+        input1 = float(request.form["input1"])
+        input2 = float(request.form["input2"])
+        input3 = float(request.form["input3"])
+        input4 = float(request.form["input4"])
+        input5 = float(request.form["input5"])
+        input6 = float(request.form["input6"])
+        input7 = float(request.form["input7"])
+        input8 = float(request.form["input8"])
+        input9 = float(request.form["input9"])
+        input10 = float(request.form["input10"])
+        input11 = float(request.form["input11"])
+
+        features = [[input1, input2, input3, input4, input5, input6, input7, input8, input9, input10, input11]]
+        print(features)
+
+        # int_features = [float(x) for x in user_wine]
+    # int_features = [float(x) for x1 in request.form.values()]
+        # final_features = [np.array(int_features)]
+        
+        model = pickle.load(open("finalized_model.pkl", "rb"))
+        prediction = model.predict(features)
+    # print("I'm in the money!")
+        print(prediction)
+    
+        if prediction[0]=="1":
+            print("wine low quality")
+            return render_template('index.html',
+                               prediction_text='Wine is of LOW quality'
                                )
-    elif prediction==2:
-        return render_template('index.html',
-                               prediction_text='Wine is of MEDIUM quality'.format(prediction),
+        elif prediction[0]=="2":
+            return render_template('index.html',
+                               prediction_text='Wine is of MEDIUM quality'
                                )                          
-    else:
-        return render_template('index.html',
-                               prediction_text='Wine is of HIGH quality'.format(prediction),
+        else:
+            return render_template('index.html',
+                               prediction_text='Wine is of HIGH quality'
                               )
+    else:
+        return render_template('index.html')
 
 
 
